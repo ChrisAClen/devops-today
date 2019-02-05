5fb347492757ffbeba151103c7caa89ee1b6c1bc
Christopher Clendinen <chrisaclendinen@gmail.com>
2019-02-05 15:43:44 -0500

    changes to reference [#163737484]

--- DevOpsToday.MSTesting/Tests/HomeControllerTests.cs
+++ DevOpsToday.MSTesting/Tests/HomeControllerTests.cs
@@ -1,25 +1,25 @@
 using DevOpsToday.Controllers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace DevOpsToday.MSTesting.Test
+namespace DevOpsToday.MSTesting.Tests
 {
   [TestClass]
   public class HomeControllerTests
   {
-      [TestMethod]
-      public void Test_IndexAction()
-      {
-        var sut = new HomeController();
+    [TestMethod]
+    public void Test_IndexAction()
+    {
+      var sut = new HomeController();
 
-        Assert.IsNotNull(sut.Index());
-      }
+      Assert.IsNotNull(sut.Index());
+    }
 
-      [TestMethod]
-      public void Test_PrivacyAction()
-      {
-        var sut = new HomeController();
+    [TestMethod]
+    public void Test_PrivacyAction()
+    {
+      var sut = new HomeController();
 
-        Assert.IsNotNull(sut.Privacy());
-      }
+      Assert.IsNotNull(sut.Privacy());
+    }
   }
 }
