da6802306ae721400caaeec76fa75b01cfbb69ab
Christopher Clendinen <chrisaclendinen@gmail.com>
2019-02-06 10:51:35 -0500

    add a new test

---
+++ DevOpsToday.MSTesting/Tests/ErrorViewModelTest.cs
@@ -0,0 +1,18 @@
+using DevOpsToday.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevOpsToday.MSTesting.Tests
+{
+  [TestClass]
+  public class ErrorViewModelTests
+  {
+    [TestMethod]
+    public void Test_ShowRequestIdMethod()
+    {
+      var sut = new ErrorViewModel(){RequestId = "chris"};
+
+      Assert.IsTrue(sut.ShowRequestId);
+    }
+  }
+    
+}
